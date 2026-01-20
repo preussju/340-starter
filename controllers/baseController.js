d6e64f6d5df6aa7051896f039008862f0c4aac83
preussju <jveiga@byupathway.edu>
2026-01-19 22:39:33 -0300

    week 3 / adding the nav inventory

---
+++ controllers/baseController.js
@@ -0,0 +1,11 @@
+//A controller is the location where the logic of the application resides
+
+const utilities = require("../utilities/")
+const baseController = {}
+
+baseController.buildHome = async function(req, res){
+  const nav = await utilities.getNav()
+  res.render("index", {title: "Home", nav})
+}
+
+module.exports = baseController
