4a58861bb0f42f4f0f643ac55d37f97704878b5a
preussju <jveiga@byupathway.edu>
2026-01-20 23:59:13 -0300

    error nessages

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
 const inventoryRoute = require("./routes/inventoryRoute")
+const utilities = require('./utilities/index');
 
 /* ***********************
  * View Engine and Templates
@@ -29,8 +30,27 @@ app.use(static)
 //app.get("/", function (req, res) {
 // res.render("index",{title:"Home"})
 //})
+//app.get("/", baseController.buildHome)  //M-V-C methodology.
+app.get("/", utilities.handleErrors(baseController.buildHome))
+// File Not Found Route - must be last route in list
+app.use(async (req, res, next) => {
+  next({status: 404, message: 'Sorry, we appear to have lost that page.'})
+})
 
-app.get("/", baseController.buildHome)  //M-V-C methodology.
+/* ***********************
+* Express Error Handler
+* Place after all other middleware
+*************************/
+app.use(async (err, req, res, next) => {
+  let nav = await utilities.getNav()
+  console.error(`Error at: "${req.originalUrl}": ${err.message}`)
+  if(err.status == 404){ message = err.message} else {message = 'Oh no! There was a crash. Maybe try a different route?'}
+  res.render("errors/error", {
+    title: err.status || 'Server Error',
+    message,
+    nav
+  })
+})
 
 /* ***********************
  * Local Server Information
