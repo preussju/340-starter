785af4505aabc0d262a4ded3ad5c0300a36ca302
preussju <jveiga@byupathway.edu>
2026-01-26 22:26:57 -0300

    adding the register view

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,7 +2,12 @@
 const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
+const utilities = require("../utilities/index")
 
-router.get("/login", accountController.buildLogin);
+//login view 
+router.get("/login", utilities.handleErrors(accountController.buildLogin));
+//registration view 
+router.get("/register", utilities.handleErrors(accountController.buildRegister));
+
 
 module.exports = router;
--- utilities/index.js
+++ utilities/index.js
@@ -91,5 +91,5 @@ Util.buildDetailGrid = async function(data){
  **************************************** */
 Util.handleErrors = fn => (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
 
-module.exports = Util
+module.exports = Util;
 
