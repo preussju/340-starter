34a521c948cfc0a5b48d3787b97d458e53900f52
preussju <jveiga@byupathway.edu>
2026-02-05 18:09:20 -0300

    task 2

--- utilities/index.js
+++ utilities/index.js
@@ -43,7 +43,7 @@ Util.buildClassificationGrid = async function(data){
       + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
       + 'details"><img src="' + vehicle.inv_thumbnail 
       +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors" /></a>'
+      +' on CSE Motors"/></a>'
       grid += '<div class="namePrice">'
       grid += '<hr />'
       grid += '<h2>'
